573a7e468ae0244ac88c180d3ebf1ecf11990094
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-24 23:57:36 +1200

    First right diagram

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,15 @@
-import matplotlib.pyblot as plt
+import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(input_values, squares, linewidth=2, color='blue')
+# diagram's name and x and y
+ax.set_title('Squares, Numbers', fontsize=24)
+ax.set_xlabel('Value', fontsize=14)
+ax.set_ylabel('Square of Value', fontsize=14)
+
+ax.tick_params(axis='both', labelsize=14)
+
 
 plt.show()
