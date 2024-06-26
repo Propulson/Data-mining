0d805bb09c054ad9d493941bc4fb59a98bf7f556
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-26 15:34:28 +1200

    Include scatters

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_values = list(range(5001))
+y_values = [x**3 for x in x_values]
 
-plt.style.use('ggplot')
+plt.style.use('dark_background')
 fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, s=10)
-ax.axis([0, 1100, 0, 1100000])
-plt.show()
+ax.scatter(x_values, y_values, c='yellow', s=10)
+ax.axis()
+plt.savefig('squares.png', bbox_inches='tight')
 #name x, y, others...
 ax.set_title('Square Numbers', fontsize=24)
 ax.set_xlabel('Value', fontsize=14)
