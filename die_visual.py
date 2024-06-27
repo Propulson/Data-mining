591b41d8d9e27ddac91e798862fb75906584e2ab
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-27 23:53:27 +1200

    Create html page with result

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,6 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
 # create cube
@@ -15,5 +18,13 @@ for value in range(1, die.sides + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+#view results
+x_values = list(range(1, die.sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
 
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='My Results', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data": data, "layout": my_layout}, filename='d6.html')
+
+print(frequencies)
