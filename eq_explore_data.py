bd81f028eab439e59cee6e24fd81a7344800a97e
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-28 20:10:54 +1200

    Using json

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -4,6 +4,9 @@ filename = 'eq_data_1_day_m1.geojson'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
-readable_file = 'eq_data_1_day_m1.geojson'
+all_eq_dicts = all_eq_data['features']
+print(len(all_eq_dicts))
+
+readable_file = 'readable_eq_data.json'
 with open(readable_file, 'w') as f:
     json.dump(all_eq_data, f, indent=4)
