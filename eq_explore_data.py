4fd93dcf59f48395dfdebf1c4fb02b87412c86b9
Propulson <158391413+Propulson@users.noreply.github.com>
2024-06-28 20:23:16 +1200

    Refactoring json

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,7 +5,19 @@ with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 print(len(all_eq_dicts))
+print(f'\033[31m{mags[:10]}\n\033[0m'
+      f'\033[32m{lons[:5]}\n\033[0m'
+      f'\033[33m{lats[:5]}\033[0m')
+
 
 readable_file = 'readable_eq_data.json'
 with open(readable_file, 'w') as f:
